0d8082b99cf7c7232fd8e63e404e2f46df4c9faa
cui1fan <1819285039@qq.com>
2025-09-14 17:16:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ print("年份\t地球体重(kg)\t月球体重(kg)")
 for year in range(1, 11):
     earth_weight = initial_weight + yearly_gain * year
     moon_weight = earth_weight * 0.165
-    print("{}\t{:.2f}\t\t{:.2f}".format(year, earth_weight, moon_weight))
+    print("{}\t{: .2f}\t\t{: .2f}".format(year, earth_weight, moon_weight))
